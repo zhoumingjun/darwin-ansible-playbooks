e25516f75d174048b05e9f9590918f069bb2b5de
zhoumingjun <zhoumingjun@gmail.com>
2018-08-26 23:07:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Overview
+**This is forked from https://github.com/siyelo/laptop.**
+
+I only use this for installing packages.
+For configurations, I prefer dotfiles.
+
+# Installation
+
+```sh  
+sh -c "$(curl -sSL https://raw.githubusercontent.com/zhoumingjun/ubuntu-devenv-playbook/master/bootstrap.sh)"
+
+```
+
+## Post-Installation
+restore dotfiles
+```sh
+sh -c "$(curl -sSL https://raw.githubusercontent.com/zhoumingjun/dotfiles/master/.bin/dotfiles-install.sh)"
+```
+
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Welcome to the my mac-devenv
+# It is forked from https://github.com/siyelo/laptop
+ 
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+fancy_echo "Boostrapping ..."
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+ 
+if ! command -v cc >/dev/null; then
+  fancy_echo "Installing xcode ..."
+  xcode-select --install 
+else
+  fancy_echo "Xcode already installed. Skipping."
+fi
+
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+else
+  fancy_echo "Homebrew already installed. Skipping."
+fi
+
+# [Install Ansible](http://docs.ansible.com/intro_installation.html).
+if ! command -v ansible >/dev/null; then
+  fancy_echo "Installing Ansible ..."
+  brew install ansible 
+else
+  fancy_echo "Ansible already installed. Skipping."
+fi
+
+ 
+ansible-playbook dev.yml -i hosts -c local   -vv
